1778029386708d9264c4167f203ba0358c6c9926
Mahamed Mahad <mahameddeeq26@hotmail.com>
2022-01-31 13:26:33 +0100

    Header-component

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+R
+"public/assets/images/tmdb_logo.svg,d\8\d8220ae79dc486e38637c20245b4c1284dc59273
+T
+$public/assets/images/search-icon.svg,d\8\d89a5dcb0bfc2d6523c483122068544324e12fd5
+Y
+)public/assets/images/react-movie-logo.svg,5\5\55865c7a4ab37c0c5555ac3de0748f6cadb87833
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+//components
+import Header from "./components/header";
+
+
+//styles
+import {GlobalStyle} from "./GlobalStyles";
 
 const App = () => {
     return (
         <div>
-            Movie APP
+            <GlobalStyle/>
+            {/***header**/}
+            <Header/>
+            {/***Hero**/}
+            {/***Main Section**/}
+            {/***Search Section**/}
         </div>
     )
 }
