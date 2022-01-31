62acddb4a791ede4b127a3a6f0c483cf3a59f503
Mahamed Mahad <mahameddeeq26@hotmail.com>
2022-01-31 14:26:24 +0100

    home-page api-configs & custom hooks

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+//config
+import {POSTER_SIZE,BACKDROP_SIZE,IMAGE_BASE_URL } from './config'
+
+
+const Home = (props) =>  {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default Home;
